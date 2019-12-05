04d8f41bbda451b91b17635482aebf04d1911264
Andrew Millar <duckman.andrew@gmail.com>
2019-12-05 23:28:00 +0100

    Add new TODO

--- src/js/main.js
+++ src/js/main.js
@@ -12,6 +12,8 @@
    * @TODO Refactor to put all the functionality insite an 
    * object (MVC anyone).
    * 
+   * @TODO Pour it all into a VUE/react mold
+   * 
    * @TODO Make the label text so that you no longer need the 
    * textarea but only have to klik on the label text and can 
    * either start typing or past a load of text
